e5ff4cc3a23bf3451c58af73a56d4f50dc4f45db
jojojo8359 <31038772+jojojo8359@users.noreply.github.com>
2024-07-17 19:30:12 -0400

    Initial commit

---
+++ nix/flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Top-level NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+  let
+    inherit (self) outputs;
+
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        allowUnfree = true;
+      };
+    };
+
+  in
+  {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs outputs; };
+
+        modules = [
+          # NixOS Configuration - based on hostname
+          ./machines/nixos/configuration.nix
+        ];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "joel@nixos" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs outputs; };
+        
+        # Home Manager configuration file
+        modules = [ ./home/home.nix ];
+      };
+    };
+  };
+}
---
+++ nix/home/home.nix
@@ -0,0 +1,20 @@
+{ inputs, config, pkgs, ... }:
+
+{
+  imports = [];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.username = "joel";
+  home.homeDirectory = "/home/joel";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+}
